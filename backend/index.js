28683f5e3768ae560bff5731fa21e497acb87bb2
Sanjay Nagarajan <89632855+sanjay-1505@users.noreply.github.com>
2025-03-09 12:17:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`/images/${req.file.filename}`
+        image_url:`https://ecommercestore-backend.onrender.com/images/${req.file.filename}`
     })
 })
 
